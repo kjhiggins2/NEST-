934df2b2f48baf9d72156611915a6e38d64b72ec
Korey Higgins <koreyjhiggins@gmail.com>
2020-10-11 12:53:01 -0400

    Changes to user reg

--- www/regform.php
+++ www/regform.php
@@ -22,7 +22,16 @@ include 'header.php'
       <br>
       <label>Street Address</label>  <input type="text" id="street" required>
       <label>City</label>   <input type="text" id="city" required>
-      <label>zip code</label> <input type="text" id="zip" required>
+      <label>zip code</label> <input type="text" id="zip" required><br>
+      <label for="school">School</label>
+      <select id="school">
+        <option value="Tollgate">Tollgate</option>
+        <option value="Pilgrim">Pilgrim</option>
+        <option value="Moses Brown">Moses Brown</option>
+        <option value="Mount St Charles"> Mount St Charles</option>
+        <option value="Lincoln"> Lincoln</option>
+        <option value="La Salle"> La Salle</option>
+      </select>
       <br>
       <h3> Parent Information</h3>
       <label> Parent First Name</label>  <input type="text" id="parent-first-name" required>
@@ -37,7 +46,7 @@ include 'header.php'
       <h3>Social Media</h3>
       <label>Instagram</label> <input type="text" id="insta" required>
       <label>twitter</label> <input type="text" id="twitter" required>
-      <br> 
+      <br>
       <h3> Informed Consent</h3>
       <p> In consideration of being allowed to participate in the activities and programs of NorthEast Sports Training and Rehabilitation, Inc, and to use its facilities, equipment, and machinery in addition to the payment of any fee or charge, I do hereby waive, release, and forever discharge NorthEast Sports Training and Rehabilitation, Inc, and its officers, agents, employees, representatives, executors, and all others from any and all responsibilities or liability for injuries or damages resulting from any participation in any activities or my use of equipment or machinery in the above mentioned facilities or arising out of my participation in any activities at said facility. I do hereby release all of those mentioned and any others acting on their behalf from any responsibility of liability for any injury or damage to myself, out of or connected with my participation in any activities at NorthEast Sports Training and Rehabilitation, Inc, or the use of any equipment at NorthEast Sports Training and Rehabilitation, Inc.
       I understand and am aware that strength, flexibility, and aerobic exercise, including the use of equipment, can be a potentially hazardous activity. I also understand that fitness activities involve a risk of injury and that I am voluntarily participating in these activities and using equipment and machinery with knowledge of the dangers involved. </p>
